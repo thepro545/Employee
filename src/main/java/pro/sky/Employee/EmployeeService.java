4b175edde8b0731d5637d7749ffd062ddca54d84
Max <thepro545@mail.ru>
2022-02-28 17:55:33 +0300

    done

--- src/main/java/pro/sky/Employee/EmployeeService.java
+++ src/main/java/pro/sky/Employee/EmployeeService.java
@@ -2,9 +2,7 @@ package pro.sky.Employee;
 
 public interface EmployeeService {
     Employee add(String firstName, String lastName);
-    Employee add(Employee employee);
     Employee remove(String firstName, String lastName);
-    Employee remove(Employee employee);
     Employee find(String firstName, String lastName);
 
 }
--- src/main/java/pro/sky/Employee/EmployeeServiceImp.java
+++ src/main/java/pro/sky/Employee/EmployeeServiceImp.java
@@ -9,47 +9,39 @@ public class EmployeeServiceImp implements EmployeeService {
     private int size;
 
     public EmployeeServiceImp() {
-        employees = new Employee[10];
+        employees = new Employee[3];
     }
 
     @Override
     public Employee add(String firstName, String lastName) {
-        Employee newEmployee = new Employee(firstName,lastName);
-        return add(newEmployee);
-    }
-
-    public Employee add(Employee employee) {
-        if(size == employees.length){
+        Employee newEmployee = new Employee(firstName, lastName);
+        if (size == employees.length) {
             throw new EmployeeBookIsFullException();
         }
-        int index = indexOf(employee);
+        int index = indexOf(newEmployee);
 
-        if(index != -1){
+        if (index != -1) {
             throw new EmployeeExistException();
         }
-        employees[size++] = employee;
-        return employee;
+        employees[size++] = newEmployee;
+        return newEmployee;
     }
 
     @Override
     public Employee remove(String firstName, String lastName) {
-        Employee newEmployee = new Employee(firstName,lastName);
-        return remove(newEmployee);
-    }
+        Employee newEmployee = new Employee(firstName, lastName);
 
-    @Override
-    public Employee remove(Employee employee) {
-        int index = indexOf(employee);
-
-        if(index != -1){
+        int index = indexOf(newEmployee);
+        if (index != -1) {
             Employee result = employees[index];
-            System.arraycopy(employees, index +1, employees, index, size - index - 1);
+            System.arraycopy(employees, index + 1, employees, index, size - index - 1);
             employees[--size] = null;
             return result;
-        } else{
+        } else {
             throw new EmployeeNotFoundException();
         }
     }
+
 
     @Override
     public Employee find(String firstName, String lastName) {
