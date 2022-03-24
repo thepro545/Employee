efc621de88416ad3317aab8c2e799bc906d64a79
Max <thepro545@mail.ru>
2022-03-24 19:27:42 +0300

    Libs

--- src/main/java/pro/sky/Employee/Service/EmployeeService.java
+++ src/main/java/pro/sky/Employee/Service/EmployeeService.java
@@ -2,10 +2,11 @@ package pro.sky.Employee.Service;
 
 import pro.sky.Employee.Data.Employee;
 
+import javax.naming.InvalidNameException;
 import java.util.Collection;
 
 public interface EmployeeService {
-    Employee addEmployee(String firstName, String lastName, int salary, int department);
+    Employee addEmployee(String firstName, String lastName, int salary, int department) throws InvalidNameException;
     Employee removeEmployee(String firstName, String lastName);
     Employee findEmployee(String firstName, String lastName);
     Collection <Employee> getAllEmployees();
